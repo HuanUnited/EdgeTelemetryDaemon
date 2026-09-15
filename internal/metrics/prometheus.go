99ed4c986df50b30eb11e4a4c2ca3d13891eea6c
HuanUnited <huanthelegend@gmail.com>
2026-09-15 16:40:04 +0700

    fix: prometheus gauge corruption

--- internal/metrics/prometheus.go
+++ internal/metrics/prometheus.go
@@ -52,8 +52,9 @@ func (m *Metric) Add(v uint64) {
 func (m *Metric) Set(v uint64) {
 	if m.Type == TypeGauge {
 		m.valBits.Store(math.Float64bits(float64(v)))
+	} else {
+		m.valBits.Add(v)
 	}
-	m.valBits.Add(v)
 }
 
 func (m *Metric) SetFloat64(v float64) {
